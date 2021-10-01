a8fd1c69f152adaab46ea4d24b8403370968b7c2
Christian Nirschl <christiannirschl6@gmail.com>
2021-09-30 23:53:56 -0700

    final attempt at local storage and readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # day-planner
+
+![image](https://user-images.githubusercontent.com/89895612/135577476-a9528a50-a92f-40e2-8b55-a50b49c22c91.png)
+
+Link to the depoyed site:
+https://chrisnirschl1.github.io/day-planner/
+
+It will change colors but the screenshots where taken after 5
+
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-var currentDay = moment().format("dddd[,] MMMM Do [at] HH:mm:ss");
-$("#currentDay").text(currentDay);
+//Set day out
+// var currentDay = moment().format("dddd[,] MMMM Do [at] HH:mm:ss");
+// $("#currentDay").text(currentDay);
 
-
+//Runs function on an interval for seconds
 setInterval (function(){
     var currentTime = moment().format("dddd[,] MMMM Do [at] HH:mm:ss")
     $("#currentDay").text(currentTime)
 
 }, 1000) 
 
-
+//Test for hours
 var hour = moment().format("HH");
 console.log(hour);
 
 
+//Variables I though I'd need
+// var hour9 = $("#9")
+// var hour10 = $("#10")
+// var hour11 = $("#11")
+// var hour12 = $("#12")
+// var hour13 = $("#13")
+// var hour14 = $("#14")
+// var hour15 = $("#15")
+// var hour16 = $("#16")
+// var hour17 = $("#17")
 
-var hour9 = $("#9")
-var hour10 = $("#10")
-var hour11 = $("#11")
-var hour12 = $("#12")
-var hour13 = $("#13")
-var hour14 = $("#14")
-var hour15 = $("#15")
-var hour16 = $("#16")
-var hour17 = $("#17")
-
+//Matches the hour to a preset time, then changes the color if it's the future or present
 function changeColor(){
     var plannerTime = moment().format("HH");
     
@@ -67,12 +69,41 @@ function changeColor(){
 }
 
 changeColor();
-
-var saveBtn = $(".savebtn")
+//Save to local storage
+var saveBtn = $(".saveBtn")
 saveBtn.on("click", function(){
-    $(this).preventDefault();
+   
     var writing = $(this).siblings(".planner").val();
     var timing = $(this).siblings(".planner").attr("id");
-
+console.log(writing)
+console.log(timing)
+localStorage.getItem(writing, timing)
 })
+//Should move it to local storage and dispay it on screen after refresh
+var nineTime = localStorage.getItem(9)
+$("#9").val(nineTime)
+
+var tenTime = localStorage.getItem(10)
+$("#10").val(tenTime)
+
+var elevenTime = localStorage.getItem(11)
+$("#11").val(elevenTime)
+
+var twelveTime = localStorage.getItem(12)
+$("#12").val(twelveTime)
+
+var thirteenTime = localStorage.getItem(13)
+$("#13").val(thirteenTime)
+
+var fourteenTime = localStorage.getItem(14)
+$("#14").val(fourteenTime)
+
+var fifteenTime = localStorage.getItem(15)
+$("#15").val(fifteenTime)
+
+var sixteenTime = localStorage.getItem(16)
+$("#16").val(sixteenTime)
+
+var seventeenTime = localStorage.getItem(17)
+$("#17").val(seventeenTime)
 
