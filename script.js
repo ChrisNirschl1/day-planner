17f3680c2b2c2948d9da30ea7341579b7d25533f
Christian Nirschl <christiannirschl6@gmail.com>
2021-09-30 20:53:20 -0700

    trying to turn the planner colors

--- script.js
+++ script.js
@@ -23,3 +23,14 @@ var hour14 = $("#14")
 var hour15 = $("#15")
 var hour16 = $("#16")
 var hour17 = $("#17")
+
+function changeColor(){
+    var plannerTime = moment().format("H");
+
+    if (plannerTime === "hour9"){
+        $("#9").addClass("present");
+        $("#10, #11, #12, #13, #14, #15, #16, #17").addClass("future")
+    };
+}
+
+console.log(changeColor)
